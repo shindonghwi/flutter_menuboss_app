3978d415047ebe5ee4178034ddb0448184b94317
shindonghwi <ehdgnl8940@naver.com>
2023-08-16 18:56:04 +0900

    feat: 스플래시 화면 제작

--- lib/presentation/features/splash/SplashScreen.dart
+++ lib/presentation/features/splash/SplashScreen.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:menuboss/navigation/Route.dart';
 import 'package:menuboss/presentation/components/Clickable/Clickable.dart';
-import 'package:menuboss/presentation/components/checkbox/checkbox/BasicBorderCheckBox.dart';
+import 'package:menuboss/presentation/ui/colors.dart';
+import 'package:menuboss/presentation/utils/Common.dart';
 
-class SplashScreen extends HookConsumerWidget {
+class SplashScreen extends HookWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isChecked = useState(false);
-
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFffffff),
+      backgroundColor: getColorScheme(context).colorBrown500,
       body: Clickable(
         onPressed: () {
-          // Navigator.pushNamedAndRemoveUntil(
-          //   context,
-          //   RoutingScreen.Login.route,
-          //       (route) => false,
-          // );
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            RoutingScreen.Login.route,
+            (route) => false,
+          );
         },
         child: Center(
-          child: BasicBorderCheckBox(
-            isChecked: isChecked.value,
-            onChange: (bool) {
-              isChecked.value = !bool;
-            },
+          child: SvgPicture.asset(
+            "assets/imgs/splash_logo.svg",
+            width: 128,
+            height: 64,
           ),
-          // child: SvgPicture.asset(
-          //   "assets/imgs/splash_logo.svg",
-          //   width: 128,
-          //   height: 64,
-          ),
+        ),
       ),
     );
   }
