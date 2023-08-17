42bd1aaf988d3e265f4e5ba696aa684cd0fe74ab
shindonghwi <ehdgnl8940@naver.com>
2023-08-17 10:53:05 +0900

    fix: checkbox component 제작

--- lib/presentation/components/checkbox/checkbox/BasicBorderCheckBox.dart
+++ lib/presentation/components/checkbox/checkbox/BasicBorderCheckBox.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:menuboss/presentation/ui/colors.dart';
+import 'package:menuboss/presentation/ui/typography.dart';
 import 'package:menuboss/presentation/utils/Common.dart';
 
-
 class BasicBorderCheckBox extends HookWidget {
+  final String label;
   final bool isChecked;
   final Function(bool) onChange;
 
   const BasicBorderCheckBox({
     super.key,
+    this.label = "",
     required this.isChecked,
     required this.onChange,
   });
@@ -38,35 +40,50 @@ class BasicBorderCheckBox extends HookWidget {
 
     return GestureDetector(
       onTap: toggleCheckbox,
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 300),
-        child: checkState.value
-            ? Align(
-          key: const Key('unchecked'),
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            width: 24,
-            height: 24,
-            "assets/imgs/icon_check.svg",
-            colorFilter: ColorFilter.mode(
-              getColorScheme(context).colorGray300,
-              BlendMode.srcIn,
-            ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 100),
+            child: checkState.value
+                ? Align(
+                    key: const Key('unchecked'),
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      width: 24,
+                      height: 24,
+                      "assets/imgs/icon_check.svg",
+                      colorFilter: ColorFilter.mode(
+                        getColorScheme(context).colorGray300,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  )
+                : Align(
+                    key: const Key('checked'),
+                    alignment: Alignment.center,
+                    child: SvgPicture.asset(
+                      width: 24,
+                      height: 24,
+                      "assets/imgs/icon_check.svg",
+                      colorFilter: ColorFilter.mode(
+                        getColorScheme(context).colorPrimary500,
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
           ),
-        )
-            : Align(
-          key: const Key('checked'),
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            width: 24,
-            height: 24,
-            "assets/imgs/icon_check.svg",
-            colorFilter: ColorFilter.mode(
-              getColorScheme(context).colorBrown500,
-              BlendMode.srcIn,
+          if (label.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: Text(
+                label,
+                style: getTextTheme(context).b1sb.copyWith(
+                      color: getColorScheme(context).black,
+                    ),
+              ),
             ),
-          ),
-        ),
+        ],
       ),
     );
   }
