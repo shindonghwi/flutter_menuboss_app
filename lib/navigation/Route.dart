543608a390d0b56998a25e4c74e5d0d9b3f11c21
shindonghwi <ehdgnl8940@naver.com>
2023-08-17 19:48:41 +0900

    feat: add tv, my plan, my profile route 추가

--- lib/navigation/Route.dart
+++ lib/navigation/Route.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:menuboss/presentation/features/add_tv/AddTvScreen.dart';
+import 'package:menuboss/presentation/features/main/my_page/plan/MyPlanScreen.dart';
+import 'package:menuboss/presentation/features/main/my_page/profile/MyProfileScreen.dart';
+
 import '../presentation/features/detail/modify/DetailTvModifyScreen.dart';
 import '../presentation/features/detail/setting/DetailTvSettingScreen.dart';
 import '../presentation/features/detail/tv/DetailTvScreen.dart';
@@ -11,13 +15,15 @@ enum RoutingScreen {
   Splash(route: "/splash"), // 스플래시
   Login(route: "/login"), // 로그인
   Main(route: "/main"), // 메인
-  AddTV(route: "/add/tv"), // 티비 추가하기
+
   DetailTv(route: "/detail/tv"), // 티비 상세
   DetailTvSetting(route: "/detail/tv/setting"), // 티비 상세 설정
   DetailTvModify(route: "/detail/tv/modify"), // 티비 정보 수정
   ScreenList(route: "/list/tv_screen"), // 스크린 리스트
-  MyPage(route: "/my_page"), // 마이페이지
-  ProfilePlan(route: "/my/profile/plan"); // 프로필 플랜 정보
+
+  AddTV(route: "/add/tv"), // 티비 추가하기
+  MyProfile(route: "/my/profile"), // 프로필 정보
+  MyProfilePlan(route: "/my/profile/plan"); // 프로필 플랜 정보
 
   final String route;
 
@@ -30,13 +36,14 @@ enum RoutingScreen {
       RoutingScreen.Splash.route: (context) => const SplashScreen(),
       RoutingScreen.Login.route: (context) => const LoginScreen(),
       RoutingScreen.Main.route: (context) => const MainScreen(),
-      // RoutingScreen.AddTV.route: (context) => const SplashScreen(),
       RoutingScreen.DetailTv.route: (context) => const DetailTvScreen(),
       RoutingScreen.DetailTvSetting.route: (context) => const DetailTvSettingScreen(),
       RoutingScreen.DetailTvModify.route: (context) => const DetailTvModifyScreen(),
-      RoutingScreen.ScreenList.route: (context) =>  const TvListScreen(),
-      // RoutingScreen.Profile.route: (context) => const SplashScreen(),
-      // RoutingScreen.ProfilePlan.route: (context) => const SplashScreen(),
+      RoutingScreen.ScreenList.route: (context) => const TvListScreen(),
+
+      RoutingScreen.AddTV.route: (context) => const AddTvScreen(),
+      RoutingScreen.MyProfile.route: (context) => const MyPlanScreen(),
+      RoutingScreen.MyProfilePlan.route: (context) => const MyProfileScreen(),
     };
   }
 
@@ -55,6 +62,12 @@ enum RoutingScreen {
       return const DetailTvModifyScreen();
     } else if (route == RoutingScreen.ScreenList.route) {
       return const TvListScreen();
+    } else if (route == RoutingScreen.AddTV.route) {
+      return const AddTvScreen();
+    } else if (route == RoutingScreen.MyProfile.route) {
+      return const MyPlanScreen();
+    } else if (route == RoutingScreen.MyProfilePlan.route) {
+      return const MyProfileScreen();
     } else {
       return const SplashScreen();
     }
