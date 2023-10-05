7c50e3212d1aab6375f782ca929bb07860ddaa03
shindonghwi <ehdgnl8940@naver.com>
2023-10-05 15:26:19 +0900

    fix: 비밀번호 정규식 패턴 변경

--- lib/presentation/utils/RegUtil.dart
+++ lib/presentation/utils/RegUtil.dart
@@ -6,7 +6,7 @@ class RegUtil{
   }
 
   static bool checkPw(String pw){
-    final regex = RegExp(r'^[a-zA-Z0-9]{8,20}$');
+    final regex = RegExp(r'^(?=.*[a-zA-Z가-힣])(?=.*\d)[a-zA-Z가-힣\d!@#$%^&*()_+,\\.<>?;:{}\[\]=\-~]{8,20}$');
     return regex.hasMatch(pw);
   }
 
